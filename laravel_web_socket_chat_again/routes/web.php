9c1325a1c929a87afd792cfebc02b4fdbaf91b43
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2022-09-16 17:21:38 +0600

    websocket setup completed

--- laravel_web_socket_chat_again/routes/web.php
+++ laravel_web_socket_chat_again/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Events\WebsocketDemoEvent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
+    broadcast( new WebsocketDemoEvent( 'some data' ) );
+
     return view('welcome');
 });
 
